de87a9228eaeb044697ce68e45a3052a3f43772b
EdgardoDev <info@edgardopintoescalier.com>
2022-07-09 18:20:29 +0200

    Added End To End tests to deployment pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,12 @@
+/* global cy */
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   video: false,
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
